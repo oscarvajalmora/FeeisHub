04b09fd7b756f75e2f2609edc7848e916fbe1347
Oscar Carvajal <oscar.carvajal@autofact.cl>
2021-03-29 18:55:04 -0300

    App name added to header icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-          <a class="navbar-brand" href="{{ route('home') }}">Rebiius</a>
+          <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation" style="border: none;">
             <span class="material-icons">menu</span>
           </button>
