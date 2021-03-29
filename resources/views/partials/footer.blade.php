a07ec6d928d7aaca6603cd4e4aebb8a84106ff7e
Oscar Carvajal <oscar.carvajal@autofact.cl>
2021-03-29 19:04:23 -0300

    Heart icon fixed on footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col text-center text-muted">
-        <p class="align-items-center d-flex justify-content-center">Hecho con	<span class="mx-1">&#10084;</span> para las comunidades de facebook</p>
+        <p class="align-items-center">Hecho con &#10084;&nbsp;&nbsp;para las comunidades de facebook</p>
       </div>
     </div>
   </div>
