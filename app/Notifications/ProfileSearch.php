b6cb6600100adead2a6149aa4e31d750493c6b74
Oscar Carvajal <oscar.carvajal@autofact.cl>
2021-04-04 17:21:00 -0400

    Add notification for slack on homepage search

---
+++ app/Notifications/ProfileSearch.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\SlackMessage;
+
+class ProfileSearch extends Notification
+{
+    use Queueable;
+    private $url;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['slack'];
+    }
+
+    public function toSlack($notifiable)
+    {
+        return (new SlackMessage)
+            ->from('Ghost', ':ghost:')
+            ->to('#bots')
+            ->content("New profile search for " . $this->url);
+    }
+
+}
